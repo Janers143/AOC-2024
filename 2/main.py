4135e1bf125272e0ddfd0cce63c1e3aac46cf735
Anthony Salinas <salinasgo_a@lfmurcie.org>
2025-01-03 23:12:20 +0100

    Solution day 2 puzzle 2

--- 2/main.py
+++ 2/main.py
@@ -42,7 +42,7 @@ def read_input(file: str) -> list[list[int]]:
     return reports
 
 
-def is_safe(report: list[int]) -> bool:
+def is_safe(report: list[int], dampener: bool = False) -> bool:
     """
     Check if a report is safe.
 
@@ -50,9 +50,14 @@ def is_safe(report: list[int]) -> bool:
     descending and that the difference between two adjacent levels is between
     1 and 3.
 
+    For the 2nd puzzle, one bad level is tolerated. A dampener is set for one use.
+
     :param list[int] report: The list of levels of the report
+    :param bool dampener: `True` if the dampener is used, `False` if not, defaults
+    to `False`
     :return bool: `True` if the report is safe, `False` if not.
     """
+    logger.debug("Checking the report safeness for report %s", report)
     # If the report contains only 1 level it is safe
     report_len = len(report)
     if report_len < 2:
@@ -73,8 +78,8 @@ def is_safe(report: list[int]) -> bool:
     while level_2 < report_len:
         # Calculate the difference between the 2 adjacent levels
         level_diff = report[level_2] - report[level_1]
-        # Check that the order is respected (multiplication by order)
-        # and that the difference is between 1 and 3
+        # Check that the order is respected (multiplication by order to get
+        # a positive difference) and that the difference is between 1 and 3
         if not 1 <= level_diff * order <= 3:
             logger.debug(
                 "Report %s is unsafe at adjacent levels %s - %s "
@@ -84,7 +89,29 @@ def is_safe(report: list[int]) -> bool:
                 report[level_2],
                 "ascending" if order == 1 else "descending",
             )
-            # If a rule is not respected, return False
+            if dampener:
+                logger.debug("Using the dampener")
+                # If the dampener for the second puzzle is set, try testing the
+                # report safeness by removing level 1 or level 2 from the report
+                report_rem_lvl_1 = report.copy()
+                report_rem_lvl_1.pop(level_1)
+                report_rem_lvl_2 = report.copy()
+                report_rem_lvl_2.pop(level_2)
+                # Treat the case of [54, 56, 54, 52, 51, 49]
+                # The predicted order might be wrong and maybe the first level
+                # should be the one to be removed
+                report_rem_lvl_0 = None
+                if level_1 >= 1:
+                    report_rem_lvl_0 = report.copy()
+                    report_rem_lvl_0.pop(level_1 - 1)
+                # Test if one of the three reports is safe
+                return (
+                    is_safe(report_rem_lvl_1)
+                    or is_safe(report_rem_lvl_2)
+                    or (is_safe(report_rem_lvl_0) if report_rem_lvl_0 else False)
+                )
+
+            # If a rule is not respected and the dampener is not set, return False
             return False
 
         # Increment levels
@@ -108,9 +135,12 @@ def puzzle1(file: str) -> int:
     # Go through each report of the list
     safe_reports = 0
     for report in reports:
+        # Check that it is safe according to puzzle 1 rules
         if is_safe(report):
             logger.debug("Report %s is safe", report)
             safe_reports += 1
+        else:
+            logger.debug("Report %s is UNSAFE", report)
 
     # Return the solution
     return safe_reports
@@ -124,10 +154,20 @@ def puzzle2(file: str) -> int:
     :return int: The puzzle solution for the given input
     """
     # Load the input
-    _ = read_input(file)
+    reports = read_input(file)
+
+    # Go through each report of the list
+    safe_reports = 0
+    for report in reports:
+        # Check that it is safe according to puzzle 2 rules
+        if is_safe(report, dampener=True):
+            logger.debug("Report %s is safe", report)
+            safe_reports += 1
+        else:
+            logger.debug("Report %s is UNSAFE", report)
 
     # Return the solution
-    return 0
+    return safe_reports
 
 
 def main() -> None:
@@ -139,7 +179,7 @@ def main() -> None:
     print(f"First part result : {res1}")
 
     ### Second part of the problem
-    res2 = puzzle2(SMALL_INPUT)
+    res2 = puzzle2(INPUT)
     print(f"Second part result : {res2}")
 
 
